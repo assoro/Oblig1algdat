4aa179d51a16c2887fcef94ae25b0821cb25706a
assoro <44671797+Poora9@users.noreply.github.com>
2019-09-18 12:50:14 +0200

    Merge branch 'master' of C:\Users\Poorani Nagendran\Documents\OsloMet 2019-2020\1. Semester\DATS2300 - Algoritmer og datastrukturer\NyAlgdatOblig1 with conflicts.

--- src/no/oslomet/cs/algdat/Oblig1/Oblig1UnitTest.java
+++ src/no/oslomet/cs/algdat/Oblig1/Oblig1UnitTest.java
@@ -16,7 +16,7 @@ class Oblig1UnitTest {
         int [] liste = {1,3,2,4,6,10,-1,8,9,18,23,22};
         Oblig1.maks(liste);
         int [] forventet = {1,2,3,4,6,-1,8,9,10,18,22,23}
-        assertEquals(Arrays.toString(forventet), Arrays.toString(liste), "Implementer maks og denne testen");
+        assertEquals(Array.toString(forventet), Arrays.toString(liste), "Implementer maks og denne testen");
     }
 
     @org.junit.jupiter.api.Test
