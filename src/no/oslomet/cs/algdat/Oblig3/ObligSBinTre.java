90ad993d5543b57da2cc248e95b0f359f1f32bbc
Rahujan Ravindran <rahujan09@hotmail.com>
2019-11-07 14:27:16 +0100

    Oppgave5: fjern()

--- src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
+++ src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
@@ -99,30 +99,42 @@ public class ObligSBinTre<T> implements Beholder<T> {
         else if (cmp > 0) { q = p; p = p.høyre; }   // går til høyre
         else break;    // den søkte verdien ligger i p
       }
-      if (p == null) return false;   // finner ikke verdi
+      if (p == null)
+          return false;   // finner ikke verdi
 
       if (p.venstre == null || p.høyre == null)  // Tilfelle 1) og 2)
       {
         Node<T> b = p.venstre != null ? p.venstre : p.høyre;  // b for barn
-        if (p == rot) rot = b;
+        if (p == rot)
+            rot = b;
+
         else if (p == q.venstre) q.venstre = b;
         else q.høyre = b;
       }
 
       else  // Tilfelle 3)
       {
-        Node<T> s = p, r = p.høyre;   // finner neste i inorden
-        while (r.venstre != null)
-        {
-          s = r;    // s er forelder til r
-          r = r.venstre;
-        }
+          Node<T> s = p, r = p.høyre;   // finner neste i inorden
+          while (r.venstre != null) {
+              s = r;    // s er forelder til r
+              r = r.venstre;
+          }
 
-        p.verdi = r.verdi;   // kopierer verdien i r til p
+          p.verdi = r.verdi;   // kopierer verdien i r til p
 
-        if (s != p) s.venstre = r.høyre;
+          if (s != p) {
+              s.venstre = r.høyre;
+              if(r.høyre != null){
+                  r.forelder.høyre = s;
+              }
 
-        else s.høyre = r.høyre;
+
+          } else {
+              s.høyre = r.høyre;
+              if(r.høyre != null){
+                  r.forelder.høyre = s;
+              }
+          }
       }
 
       antall--;   // det er nå én node mindre i treet
